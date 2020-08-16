e17daca6f5154b5fc5b0346c7665de062ee5b81a
Xeladaren <xeladaren@pm.me>
2020-08-16 11:50:08 +0200

    - ajout list command - ajout co/deco message

--- Discord.py
+++ Discord.py
@@ -1,22 +1,57 @@
 import discord
 import config
 import Minecraft
+import asyncio
 
 client = discord.Client()
 
-def senPlayerMsg(pseudo, msg):
+def sendPlayerMsg(pseudo, msg):
+
+    print("[INFO] say on discord : <"+pseudo+"> "+msg)
+
     webhook = discord.Webhook.from_url(config.webhookURL, adapter=discord.RequestsWebhookAdapter())
     webhook.send(msg, username=pseudo)
 
+
 def startBot():
     client.run(config.token)
+
+def sendBotMsg(msg):
+    for channel in client.get_all_channels():
+        if channel.name == config.channelName :
+            future = asyncio.run_coroutine_threadsafe(channel.send(msg), client.loop)
+            future.result()
+
+async def parseCommands(message) :
+    print("[INFO] discord command :", message.content)
+    command = message.content.split(" ")
+
+    if ( "list" in command or "liste" in command ) and message.channel.name == config.channelName:
+        playersList = Minecraft.getPlayersList()
+        msg = "Il y a "+str(playersList["count"])
+        if playersList["count"] > 1:
+            msg += " joueurs "
+        else :
+            msg += " joueur "
+        msg += "connecté sur "+str(playersList["max"])
+
+        if playersList["count"] > 0 :
+            msg += " : \n"
+
+        for player in playersList["list"] :
+            msg += "\t - "+player+"\n"
+
+        await message.channel.send(msg)
 
 @client.event
 async def on_message(message):
 
     if not message.author.bot :
 
-        if message.channel.name == config.channelName:
+        if client.user in message.mentions:
+            await parseCommands(message)
+
+        elif message.channel.name == config.channelName:
 
             guildMember = message.guild.get_member(message.author.id)
 
@@ -24,6 +59,5 @@ async def on_message(message):
             if guildMember.nick :
                 pseudo = guildMember.nick
 
-            msg = message.content
-
-            Minecraft.sayOnChat(pseudo, msg)
+            for msgLine in message.content.split("\n") :
+                Minecraft.sayOnChat(pseudo, msgLine)
--- Minecraft.py
+++ Minecraft.py
@@ -5,6 +5,8 @@ import Discord
 
 import config
 
+__playersListInfo = {"lastUpdate": 0,"count": 0, "max": 0, "list": []}
+
 def __minecraftLogParser():
 
     lastModifOnLogs = 0
@@ -43,6 +45,8 @@ def __minecraftLogParser():
 
                     if "Async Chat Thread" in line :
                         __minecraftChatParser(line)
+                    elif "Server thread/INFO" in line :
+                        __minecraftServerInfoParser(line)
 
 
         else :
@@ -69,16 +73,88 @@ def __minecraftChatParser(rawMsg):
         pseudo = rawMsg[pseudoStartIndex : pseudoEndIndex]
         msg = rawMsg[msgStartIndex : ]
 
-        Discord.senPlayerMsg(pseudo, msg)
+        Discord.sendPlayerMsg(pseudo, msg)
+
+def __minecraftServerInfoParser(rawMsg):
+    if "Server thread/INFO" in rawMsg :
+        startIndex = rawMsg.index("INFO") + 7
+        rawMsg = rawMsg[startIndex:]
+
+        if "players online" in rawMsg :
+            __minecraftListParser(rawMsg)
+        elif "joined the game" in rawMsg:
+            __minecraftConnectParser(rawMsg)
+        elif "left the game" in rawMsg:
+            __minecraftConnectParser(rawMsg)
+
+def __minecraftListParser(rawMsg):
+    splitText = rawMsg.split(":")
+
+    if len(splitText) >= 2 and len(splitText[0].split(" ")) >= 8 :
+        playersCount = int(splitText[0].split(" ")[2])
+        playersMax = int(splitText[0].split(" ")[7])
+    else :
+        playersCount = 0
+        playersMax = 0
+
+    if len(splitText) >= 2 :
+        playersList = splitText[1].split(",")
+        newPlayersList = []
+        for player in playersList :
+            newPlayersList += [player.split(" ")[-1]]
+        playersList = newPlayersList
+    else :
+        playersList = []
+
+
+    __playersListInfo["lastUpdate"] = time.time()
+    __playersListInfo["count"] = playersCount
+    __playersListInfo["max"] = playersMax
+    __playersListInfo["list"] = playersList
+
+def __minecraftConnectParser(rawMsg):
+    splitMsg = rawMsg.split(" ")
+
+    pseudo = splitMsg[-4]
+    type = splitMsg[-3]
+
+    msg = ""
+    if type == "left":
+        msg += "**" + pseudo + "** a quitté le jeu"
+        print("[INFO] "+msg)
+        Discord.sendBotMsg(msg)
+    elif type == "joined":
+        msg += "**" + pseudo + "** a rejoint le jeu"
+        print("[INFO] "+msg)
+        Discord.sendBotMsg(msg)
+
 
 def startLogParser():
     _thread.start_new_thread(__minecraftLogParser, ())
 
+def getPlayersList():
+    playerInfo = {"count": 0, "max": 0, "list": []}
+
+    now = time.time()
+    executeCmd("list")
+
+    while __playersListInfo["lastUpdate"] < now:
+        time.sleep(0.1)
+
+    playerInfo["count"] = __playersListInfo["count"]
+    playerInfo["max"] = __playersListInfo["max"]
+    playerInfo["list"] = __playersListInfo["list"]
+
+    return playerInfo
+
 def sayOnChat(pseudo, msg):
+
+    print("[INFO] say on minecraft : [Discord]<"+pseudo+"> "+msg)
 
     msg = msg.replace("\\", "\\"*8)
     msg = msg.replace("\"", "\\"*4+"\"")
     msg = msg.replace("`", "\\`")
+    msg = msg.replace("^", "\\^")
 
     tellRawArg  = "[\"\","
     tellRawArg += "{\"text\":\"[\"},"
@@ -91,4 +167,7 @@ def sayOnChat(pseudo, msg):
     os.system(bashCMD)
 
 def executeCmd(cmd):
-    print("CMD : "+cmd)
+    print("[INFO] execute minecraft command : "+cmd)
+
+    bashCMD = "screen -S minecraft -X stuff \""+cmd+"\n\""
+    os.system(bashCMD)
